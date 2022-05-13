0f8b57f988c5616fc954f3ccd172765ad67a601c
joshsikder <jsikder@ad.unc.edu>
2022-05-13 15:29:38 -0400

    Updated simulations as follows: 1) simulates simple introgression of 10% from Dsantomea to Derecta 2) simulates no introgression under same parameters otherwise 3) simulates simple introgression of 10% from Dyakuba to Derecta
    This follows group meeting Friday May 13

--- phylo1/sim2.py
+++ phylo1/sim2.py
@@ -1,37 +1,47 @@
-import msprime, newick
-from IPython.display import SVG, display
+import msprime
 
-def firstSim(seqlength):
-    time_units = 1000/25 # kya -> generations
-    demo1 = msprime.Demography()
-    N=10**4
-    demo1.add_population(name="Dsantomea", initial_size=N)
-    demo1.add_population(name="Dyakuba", initial_size=N)
-    demo1.add_population(name="Derecta", initial_size=N)
-    demo1.add_population_split(time=5, derived=["Dsantomea","Dyakuba"],ancestral="Derecta")
+def ancestrySim(seqlength, randomseed):
+    # time_units = 4 # kya -> generations
+    speciesTree = msprime.Demography()
+    N=1000000 #Ne
+    time_units = 4*N
+    speciesTree.add_population(name="Dsantomea", initial_size=N)
+    speciesTree.add_population(name="Dyakuba", initial_size=N)
+    speciesTree.add_population(name="Derecta", initial_size=N)
+    speciesTree.add_population(name="santomea_yakuba", initial_size=N)
+    speciesTree.add_population(name="ancestor", initial_size=N)
+    #speciesTree.add_mass_migration(time=0.1*time_units, source="Dsantomea", dest="Derecta", proportion=.1)
+    speciesTree.add_population_split(time=0.5*time_units, derived=["Dsantomea","Dyakuba"],ancestral="santomea_yakuba")
+    speciesTree.add_population_split(time=1*time_units, derived=["santomea_yakuba","Derecta"],ancestral="ancestor")
+    
     treeseq = msprime.sim_ancestry(
-        recombination_rate=1e-6,
+        recombination_rate=1e-8,
+        # recombination_rate = 0,
         sequence_length=seqlength,
         samples=[
             msprime.SampleSet(1, ploidy=1, population="Dsantomea"),
             msprime.SampleSet(1, ploidy=1, population="Dyakuba"),
             msprime.SampleSet(1, ploidy=1, population="Derecta")
         ],
-        demography=demo1,
+        demography=speciesTree,
         record_migrations=True,
-        random_seed=123456
-    )
-    print(f"Simulation of {seqlength/10**3}Kb run, using record_migrations=True")
-    print(
-        "NB: time diff from Neanderthal split to admixture event is",
-        f"{300 * time_units - 50 * time_units:.0f} gens",
-        f"({(300 * time_units - 50 * time_units) / 2 / N} coalescence units)"
+        random_seed=randomseed
     )
     return treeseq
 
-ts = firstSim(30 * 10**3)
+ts = ancestrySim(100, 123456)
+model = msprime.MatrixMutationModel(
+    ["A","T","G","C"],
+    root_distribution = [0.25, 0.25, 0.25, 0.25],
+    transition_matrix = [[0.0, 1/3, 1/3, 1/3],
+                         [1/3, 0.0, 1/3, 1/3],
+                         [1/3, 1/3, 0.0, 1/3],
+                         [1/3, 1/3, 1/3, 0.0]]
+)
+msprime.sim_mutations(ts, rate=2, model=model)
 ts.write_fasta('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/nointro.fasta')
-with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/nointro.draw') as outfile:
-    outfile.write(ts.draw_text())
-with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/nointro.stats') as outfile:
-    outfile.write(ts)
+def write_output():
+    with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/nointro.draw', "w+") as outfile:
+        outfile.write(ts.draw_text())
+write_output()
+#print(ts.draw_text())
--- phylo1/sim3.py
+++ phylo1/sim3.py
@@ -1,51 +1,47 @@
-import msprime, newick
-from IPython.display import SVG, display
+import msprime
 
-def firstSim(seqlength):
-    time_units = 1000/25 # kya -> generations
-    demo1 = msprime.Demography()
-    N=1000
-    demo1.add_population(name="A", initial_size=N)
-    demo1.add_population(name="B", initial_size=N)
-    demo1.add_population(name="C", initial_size=N)
-    demo1.add_population(name="D", initial_size=N)
-    demo1.add_population(name="E", initial_size=N)
-    demo1.add_population(name="F", initial_size=N)
-    demo1.add_population_split(time=50, derived=["ABC","DEF"],ancestral="ABCDEF")
-    demo1.add_population_split(time=30, derived=["AB","C"],ancestral="ABC")
-    demo1.add_population_split(time=25, derived=["D","EF"],ancestral="DEF")
-    demo1.add_population_split(time=10, derived=["A","B"],ancestral="AB")
-    demo1.add_population_split(time=10, derived=["E","F"],ancestral="EF")
-    demo1.add_mass_migration(time=5, source="B", dest="C", proportion=.02)
-    demo1.add_mass_migration(time=25, source="ABC", dest="D", proportion=.07)
-    demo1.add_mass_migration(time=15, source="D", dest="C", proportion=.1)
-
+def ancestrySim(seqlength, randomseed):
+    # time_units = 4 # kya -> generations
+    speciesTree = msprime.Demography()
+    N=1000000 #Ne
+    time_units = 4*N
+    speciesTree.add_population(name="Dsantomea", initial_size=N)
+    speciesTree.add_population(name="Dyakuba", initial_size=N)
+    speciesTree.add_population(name="Derecta", initial_size=N)
+    speciesTree.add_population(name="santomea_yakuba", initial_size=N)
+    speciesTree.add_population(name="ancestor", initial_size=N)
+    speciesTree.add_mass_migration(time=0.1*time_units, source="Dyakuba", dest="Derecta", proportion=.1)
+    speciesTree.add_population_split(time=0.5*time_units, derived=["Dsantomea","Dyakuba"],ancestral="santomea_yakuba")
+    speciesTree.add_population_split(time=1*time_units, derived=["santomea_yakuba","Derecta"],ancestral="ancestor")
+    
     treeseq = msprime.sim_ancestry(
-        recombination_rate=1e-16,
+        recombination_rate=1e-8,
+        # recombination_rate = 0,
         sequence_length=seqlength,
         samples=[
-            msprime.SampleSet(1, ploidy=1, population="A"),
-            msprime.SampleSet(1, ploidy=1, population="B"),
-            msprime.SampleSet(1, ploidy=1, population="C"),
-            msprime.SampleSet(1, ploidy=1, population="D"),
-            msprime.SampleSet(1, ploidy=1, population="E"),
-            msprime.SampleSet(1, ploidy=1, population="F")
+            msprime.SampleSet(1, ploidy=1, population="Dsantomea"),
+            msprime.SampleSet(1, ploidy=1, population="Dyakuba"),
+            msprime.SampleSet(1, ploidy=1, population="Derecta")
         ],
-        demography=demo1,
+        demography=speciesTree,
         record_migrations=True,
-        random_seed=123456
-    )
-    print(f"Simulation of {seqlength/10**3}Kb run, using record_migrations=True")
-    print(
-        "NB: time diff from Neanderthal split to admixture event is",
-        f"{300 * time_units - 50 * time_units:.0f} gens",
-        f"({(300 * time_units - 50 * time_units) / 2 / N} coalescence units)"
+        random_seed=randomseed
     )
     return treeseq
 
-ts = firstSim(1 * 10**3)
-ts.write_fasta('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/multiintro.fasta')
-with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/multiintro.draw') as outfile:
-    outfile.write(ts.draw_text())
-with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/multiintro.stats') as outfile:
-    outfile.write(ts)
+ts = ancestrySim(100, 123456)
+model = msprime.MatrixMutationModel(
+    ["A","T","G","C"],
+    root_distribution = [0.25, 0.25, 0.25, 0.25],
+    transition_matrix = [[0.0, 1/3, 1/3, 1/3],
+                         [1/3, 0.0, 1/3, 1/3],
+                         [1/3, 1/3, 0.0, 1/3],
+                         [1/3, 1/3, 1/3, 0.0]]
+)
+msprime.sim_mutations(ts, rate=2, model=model)
+ts.write_fasta('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/bintro.fasta')
+def write_output():
+    with open('/nas/longleaf/home/jsikder/schriderlab/ignored/phylo1/bintro.draw', "w+") as outfile:
+        outfile.write(ts.draw_text())
+write_output()
+#print(ts.draw_text())
